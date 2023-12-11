3b3efcb047110507b3308b01be87289bd35032aa
shirguin <shirguin@yandex.ru>
2023-12-11 19:43:05 +0300

    Initial

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1); //Включение строгой типизации
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    use NewsTrait;
+
+    public function index()
+    {
+        return \view("news.index", [
+            "news" => $this->getNews(),
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        return $this->getNews($id);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['prefix' => ''], static function () {
+    Route::get('/news', [NewsController::class, 'index'])
+        ->name('news');
+    Route::get('/news/{id}/show', [NewsController::class, 'show'])
+        ->where('id', '\d+')
+        ->name('news.show');
+});
+
+
